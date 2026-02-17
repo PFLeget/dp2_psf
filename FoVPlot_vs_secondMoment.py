fix api and units

FoVPlot_vs_secondMoment.py
@@ -80,7 +80,8 @@ def plot_FoV_second_Moment(bands='g', rep="data/", repOutPlot='plots/',
                 coord = np.array([dic[visit]['xCCD'], dic[visit]['yCCD']]).T
                 if ccd not in meanifyStream:
                     if not statisticsMedian:
-                        meanifyStream.update({ccd: treegp.MeanifyStream(bin_spacing=bin_spacing, bounds=(0, 4100, 0, 4100))})
+                        # New API: meanify with bounds enables streaming mode for statistics="mean"
+                        meanifyStream.update({ccd: treegp.meanify(bin_spacing=bin_spacing, statistics="mean", bounds=(0, 4100, 0, 4100))})
                     else:
                         meanifyStream.update({ccd: treegp.meanify(bin_spacing=bin_spacing, statistics='median')})
                 meanifyStream[ccd].add_field(coord[filtering], dic[visit][key_second_moment][filtering])

SkyPlot_vs_secondMoment.py
@@ -72,8 +72,8 @@ def plot_Sky_second_Moment(bands='g', rep="data/", repOutPlot='plots/',
             if psf_max_value > 0:
                 filtering &= (dic[visit]["psf_max_value"] > psf_max_value)
 
-            # Sky coordinates (RA, Dec in degrees)
-            coord = np.array([dic[visit]['ra'], dic[visit]['dec']]).T
+            # Sky coordinates (RA, Dec) - convert from radians to degrees
+            coord = np.array([np.degrees(dic[visit]['ra']), np.degrees(dic[visit]['dec'])]).T
 
             meanifyHealpix.add_field(coord[filtering], dic[visit][key_second_moment][filtering])
 